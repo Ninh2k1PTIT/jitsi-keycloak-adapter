71fc853d6e9daf119383a5b86f323c3dfe0027ec
Ninh2k1PTIT <nguyentienhaininh@gmail.com>
2024-08-05 10:45:34 +0700

    fix: config

--- config.ts
+++ config.ts
@@ -1,17 +1,17 @@
 // keycloak
 export const KEYCLOAK_ORIGIN = Deno.env.get("KEYCLOAK_ORIGIN") ||
-  "https://ucs-sso-ng.mydomain.corp";
+  "https://iam.cmcati.vn";
 export const KEYCLOAK_ORIGIN_INTERNAL =
   Deno.env.get("KEYCLOAK_ORIGIN_INTERNAL") || KEYCLOAK_ORIGIN;
-export const KEYCLOAK_REALM = Deno.env.get("KEYCLOAK_REALM") || "ucs";
-export const KEYCLOAK_CLIENT_ID = Deno.env.get("KEYCLOAK_CLIENT_ID") || "jitsi";
+export const KEYCLOAK_REALM = Deno.env.get("KEYCLOAK_REALM") || "CIST_1";
+export const KEYCLOAK_CLIENT_ID = Deno.env.get("KEYCLOAK_CLIENT_ID") || "c-meet-online";
 export const KEYCLOAK_MODE = Deno.env.get("KEYCLOAK_MODE") || "query";
 
 // jwt
 export const JWT_ALG = Deno.env.get("JWT_ALG") || "HS256";
 export const JWT_HASH = Deno.env.get("JWT_HASH") || "SHA-256";
-export const JWT_APP_ID = Deno.env.get("JWT_APP_ID") || "myappid";
-export const JWT_APP_SECRET = Deno.env.get("JWT_APP_SECRET") || "myappsecret";
+export const JWT_APP_ID = Deno.env.get("JWT_APP_ID") || "app_id";
+export const JWT_APP_SECRET = Deno.env.get("JWT_APP_SECRET") || "app_secret";
 export const JWT_EXP_SECOND = Number(Deno.env.get("JWT_EXP_SECOND") || 3600);
 
 // adapter
